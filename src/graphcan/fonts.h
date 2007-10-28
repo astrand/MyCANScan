0ee7d12bf88dceb4ba219d7b19f9beb0e784caed
astrand <astrand@cendio.se>
2007-10-28 21:39:12 +0000

    Moved font stuff to separate file

---
+++ src/graphcan/fonts.h
@@ -0,0 +1,14 @@
+
+#define	FONTMAP_WIDTH	176
+#define	FONTMAP_HEIGHT	31
+#define	FONTMAP_COLORS	10
+
+typedef struct _fontmetrics
+{
+    char letter;
+    int x;
+    int y;
+    int width;
+    int height;
+} fontmetrics;
+
